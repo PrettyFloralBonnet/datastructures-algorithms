777aadda3b048de2e5b89598a89759a256f154f0
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-08-20 00:44:07 +0200

    merge lists in a pythonic way

--- datastructures/arrays/mergeSortedArrays.py
+++ datastructures/arrays/mergeSortedArrays.py
@@ -1,22 +1,6 @@
 
-def merge_sorted_arrays(arr1, arr2):
-    merged_array = []
-    arr1_item = arr1[0]
-    arr2_item = arr2[0]
-    i, j = 1, 1
+def merge_sorted_lists(list1, list2):
+    return sorted(list1 + list2)
 
-    if len(arr1) == 0:
-        return arr2
-    
-    if len(arr2) == 0:
-        return arr1
-
-    while arr1_item or arr2_item:
-        if not arr2_item or arr1_item <= arr2_item:
-            merged_array.append(arr1_item)
-            arr1_item = arr1[i]
-            i += 1
-        else:
-            merged_array.append(arr2_item)
-
-merge_sorted_arrays([0, 3, 4, 31], [4, 6, 30]);
+if __name__ == '__main__':
+    merge_sorted_lists([0, 3, 4, 31], [4, 6, 30])

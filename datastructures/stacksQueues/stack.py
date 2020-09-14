6640ada1d769729fa987930b51af999e832a500f
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-09-14 23:55:26 +0200

    add linked list implementation of stack in python

---
+++ datastructures/stacksQueues/stack.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# implementation using a linked list
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def __len__(self):
+        return self.length
+
+    def peek(self):
+        return self.top
+
+    def push(self, value):
+        PREVIOUS_TOP = self.top
+        self.top = Node(value)
+        if len(self):
+            self.bottom = self.top
+        self.top.next = PREVIOUS_TOP
+        self.length += 1
+        return self
+
+    def pop(self):
+        POPPED_ITEM = self.top
+        if POPPED_ITEM is None:
+            return
+        self.top = self.top.next
+        self.length -= 1
+        if len(self):
+            self.bottom = None
+        return POPPED_ITEM
+
+    def isEmpty(self):
+        if self.top is None:
+            return True
+        return False

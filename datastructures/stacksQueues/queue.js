888092a666fa1acca031399a67e9c068860498b8
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-09-19 15:49:11 +0200

    implement a queue using a linked list

--- datastructures/stacksQueues/queue.js
+++ datastructures/stacksQueues/queue.js
@@ -8,18 +8,36 @@ class Queue {
     }
 
     peek() {
-
+        return this.first;
     }
 
-    enqueue() {
+    enqueue(value) {
+        const newNode = new Node(value);
 
+        if (this.length === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
     }
 
     dequeue() {
+        if (this.length === 0) return null;
+        if (this.length === 1) this.last = null;
 
+        const first = this.first;
+        this.first = this.first.next;
+
+        this.length--;
+        return first;
     }
 
     isEmpty() {
-
+        if (this.length === 0) return true;
+        return false;
     }
 }

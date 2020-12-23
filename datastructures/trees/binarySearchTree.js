9be97e01df4f9c11380fa3f59144b208717f1291
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2020-12-24 00:08:43 +0100

    implement insert method

--- datastructures/trees/binarySearchTree.js
+++ datastructures/trees/binarySearchTree.js
@@ -12,11 +12,33 @@ class BinarySearchTree {
     }
 
     insert(value) {
+        const newNode = new Node(value);
 
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                } else {  // values equal to current value go to the right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
     }
 
     remove(value) {
-        
+
     }
 
     lookup(value) {

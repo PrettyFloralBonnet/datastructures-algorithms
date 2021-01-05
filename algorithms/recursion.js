89b948f6e2c0d682162e4e24f5d4b6bc09aebc70
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-01-05 23:41:34 +0100

    implement the iterative solution for the fibonacci exercise

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -1,4 +1,4 @@
-// factorial of the given number
+// return factorial of the given number
 
 function findFactorialRecursive(number) {
     if (number <= 2) return number;
@@ -13,12 +13,24 @@ function findFactorialIterative(number) {
     return answer;
 }
 
-// the number that appears in the fibonacci sequence under the given index
+// return the number that appears in the fibonacci sequence
+// (0, 1, 1, 2, 3, 5, 8, 13, 21, 34...) under the given index
 
-function fibonacciRecursive() {
+function fibonacciRecursive(index) {
 
 }
 
-function fibonacciIterative() {
+function fibonacciIterative(index) {
+    let firstNumber = 0, secondNumber = 1;
 
+    if (index === 0) {
+        return firstNumber;
+    } else if (index <= 2) return secondNumber;
+
+    for (let count = 0; count !== index - 1; count++) {
+        thirdNumber = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = thirdNumber;
+    }
+    return thirdNumber;
 }

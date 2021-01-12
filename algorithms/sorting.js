9cfc44a9ac050a2bf2d6013b77e6f59da63ca18a
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-01-12 17:16:13 +0100

    simplify bubble sort by getting rid of unnecessary variable

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -1,9 +1,8 @@
 const numbers = [88, 46, 7, 9, 12, 83, 47, 65, 32, 2, 80];
 
 function bubbleSort(arr) {
-    const length = arr.length;
-    for (let i = 0; i < length; i++) {
-        for (let j = 0; j < length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];

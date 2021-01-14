c7df87009619d555dd6e482c0490d933110e6d02
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-01-14 22:25:25 +0100

    add merge sort example implementation

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -39,3 +39,31 @@ function insertionSort(arr) {  // best case is O(n)
         }
     }
 }
+
+function _merge(left, right) {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right.rightIndex);
+            rightIndex++;
+        }
+    }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return _merge(mergeSort(left), mergeSort(right));
+}

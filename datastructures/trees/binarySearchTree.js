03d21a72a7974c05d8a9f6ef00847b9d20095be2
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-01-24 17:52:48 +0100

    implement BFS recursively

--- datastructures/trees/binarySearchTree.js
+++ datastructures/trees/binarySearchTree.js
@@ -137,4 +137,26 @@ class BinarySearchTree {
         }
         return list;
     }
+
+    breadthFirstSearchRecursive(queue, list) {
+        if (!queue.length) {
+            return list;
+        }
+
+        let currentNode = queue.shift();
+        list.push(currentNode.value);
+
+        if (currentNode.left) {
+            queue.push(currentNode.left);
+        }
+        if (currentNode.right) {
+            queue.push(currentNode.right);
+        }
+
+        return this.breadthFirstSearchRecursive(queue, list);
+    }
+
+    depthFirstSearch() {
+
+    }
 }

0488b41fb7ead28ebd5c3cf97a4440be3a957a3c
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-01-29 22:43:05 +0100

    move the cache inside the scope of the function

--- algorithms/dynamic-programming/dynamic.md
+++ algorithms/dynamic-programming/dynamic.md
@@ -2,4 +2,4 @@
 Dynamic programming is basically a buzzword for an **optimization technique** that takes advantage of **caching** - saving solutions to problems for when the same problem occurs again.
 
 ## Memoization
-**Memoization** is a specific form of caching that involves caching the return value of a function based on its parameters.
+**Memoization** is a specific form of caching that involves caching the return value of a function based on its parameters. It's good practice to have the cache live in the memory of the function itself (to avoid pollution of the global namespace).
--- algorithms/dynamic-programming/memoization.js
+++ algorithms/dynamic-programming/memoization.js
@@ -3,15 +3,22 @@ function arbitrary(n) {
     return n * 2;
 }
 
-let cache = {}
-function memoizedArbitrary(n) {
-    if (n in cache) {
-        console.log('Fetching from cache...');
-        return cache[n];
-    } else {
-        // again, we are assuming this takes a long time
-        console.log('Actually calculating...');
-        cache[n] = n * 2;
-        return cache[n];
+function memoizedArbitrary() {
+    let cache = {};
+    return function(n) {
+        if (n in cache) {
+            console.log('Fetching from cache...');
+            return cache[n];
+        } else {
+            // again, we are assuming this takes a long time
+            console.log('Actually calculating...');
+            cache[n] = n * 2;
+            return cache[n];
+        }
     }
 }
+
+const memoized = memoizedArbitrary()
+
+console.log(memoized(50));
+console.log(memoized(50));

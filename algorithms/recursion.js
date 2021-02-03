a1ef839578712816f4a20de21b9336c64528fe34
PrettyFloralBonnet <pawel.m.przytula@gmail.com>
2021-02-03 23:01:02 +0100

    add a fibonacci exercise using a bottom up approach

--- algorithms/recursion.js
+++ algorithms/recursion.js
@@ -58,6 +58,14 @@ console.log(fibMem(20));
 console.log(fibMem(20));
 
 
+function fibonacciBottomUp() {
+    let answer = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        answer.push(answer[i - 2] + answer[i - 1]);
+    }
+    return answer.pop();
+}
+
 // reverse a string
 
 function reverseString(str) {
